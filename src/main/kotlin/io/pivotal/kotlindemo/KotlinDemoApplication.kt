cb2d6705c06ea4cfceaefcaddf809660f0030594
Alex Basson <alexbasson@gmail.com>
2019-04-24 20:56:13 -0400

    Initial commit

---
+++ src/main/kotlin/io/pivotal/kotlindemo/KotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.kotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDemoApplication>(*args)
+}
